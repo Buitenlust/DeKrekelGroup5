458c3f537767ed71ecb2365d757cab1bdd2a799c
Buitenlust <sven@vanlangendyck.be>
2015-02-14 14:39:59 +0100

    Fundering project.

---
+++ DeKrekelGroup5/DeKrekelGroup5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeKrekelGroup5.MvcApplication" Language="C#" %>
---
+++ DeKrekelGroup5/DeKrekelGroup5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DeKrekelGroup5.Startup))]
+namespace DeKrekelGroup5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
