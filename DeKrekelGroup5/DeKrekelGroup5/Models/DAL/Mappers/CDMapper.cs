db136df96e73e4f59584f7fdcd0c981eeb519da5
MathiasPodevijn <mathias.poedevijn@hotmail.be>
2015-03-26 07:02:11 +0100

    CD/DVD mappers

---
+++ DeKrekelGroup5/DeKrekelGroup5/Models/DAL/Mappers/CDMapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using DeKrekelGroup5.Models.Domain;
+
+namespace DeKrekelGroup5.Models.DAL.Mappers
+{
+    public class CDMapper : EntityTypeConfiguration<CD>
+    {
+        //table
+            ToTable("CD");
+
+        //properties
+        Property(c => c.Omschrijving).HasMaxLength(1023);
+        Property(c => c.Titel).HasMaxLength(45).IsRequired();
+        Property(c => c.Uitgever).HasMaxLength(45)
+
+        //Primary Key
+        HasKey(c => c.Exemplaar);
+    }
+}
---
+++ DeKrekelGroup5/DeKrekelGroup5/Models/DAL/Mappers/DVDMapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace DeKrekelGroup5.Models.DAL.Mappers
+{
+    public class DVDMapper : EntityTypeConfiguration<DVD>
+    {
+        //table
+            ToTable("DVD");
+
+        //properties
+        Property(d => d.Omschrijving).HasMaxLength(1023);
+        Property(d => d.Titel).HasMaxLength(45).IsRequired();
+        Property(d => d.Uitgever).HasMaxLength(45)
+
+        //Primary Key
+        HasKey(d => d.Exemplaar);
+    }
+}
