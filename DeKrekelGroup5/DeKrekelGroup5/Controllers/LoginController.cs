acd01b275d579fce328e3e92efd467f1bff17ea1
thorlor <jonananas@live.be>
2015-04-02 15:20:57 +0200

    login zonder binding

---
+++ DeKrekelGroup5/DeKrekelGroup5/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using DeKrekelGroup5.ViewModel;
+using DeKrekelGroup5.Models.Domain;
+using System.Collections.Generic;
+
+namespace DeKrekelGroup5.Controllers
+{
+   
+    public class LoginController : Controller
+    {
+        private IGebruikerRepository GebruikersRep { get; set; }
+        private IEnumerable<Gebruiker> gebruikers;
+
+        public LoginController(IGebruikerRepository gebruikersrep)
+        {
+            GebruikersRep = gebruikersrep;
+            gebruikers = gebruikersrep.GetGebruikers();
+        }
+
+        //
+        // GET: /Account/Login
+        [HttpGet]
+        public ActionResult Login(string username)
+        {
+
+            return View(new LoginViewModel() { Username = username });
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel logon)
+        {
+            TempData["Info"] = logon.Paswoord;
+            Gebruiker gebruiker = gebruikers.SingleOrDefault(g => g.GebruikersNaam == logon.Username);
+            if (gebruiker != null && gebruiker.PaswoordHashed == gebruiker.HashPassword(logon.Paswoord)) 
+            {
+                //moet nu gebruiker binden
+                return RedirectToAction("Index", "Home");
+            }
+            return RedirectToAction("Login");
+        }
+    }
+}
+
+
+
+
---
+++ DeKrekelGroup5/DeKrekelGroup5/ViewModel/LoginViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DeKrekelGroup5.ViewModel
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Tes vandoen")]
+        [MaxLength(10, ErrorMessage = " max 10 tekens")]
+        [MinLength(6, ErrorMessage = " min 6 tekens")]
+        public string Paswoord { get; set; }
+
+        public LoginViewModel()
+        {
+
+        }
+        public LoginViewModel(string username, string paswoord)
+        {
+            Username = username;
+            Paswoord = paswoord;
+        }
+
+
+    }
+}
+
+
+
